287edacb3757fe6435aca242019ad8e42a20d192
oasis.wang <oasis.wang1016@gmail.com>
2022-06-12 12:32:03 +0800

    VitePractite

---
+++ vite-project/src/views/Layout.vue
@@ -0,0 +1,15 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import List from '../components/List.vue'
+</script>
+
+<template>
+    <a-layout>
+    <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%', background:'#4CC764' }">找美食</a-layout-header>
+    <a-layout-content :style="{ padding: '0 50px', marginTop: '64px' }"><List /></a-layout-content>
+    <a-layout-footer :style="{background:'#4CC764'}">Footer</a-layout-footer>
+  </a-layout>
+</template>
+ 
+ 
